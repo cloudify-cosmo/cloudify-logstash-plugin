f529e37fca831ba76e9418824cc949c7393cb61c
earthmant <earthmant@icloud.com>
2015-07-08 16:48:30 +0300

    added the install and uninstall tests

--- logstash_plugin/tests/test_plugin.py
+++ logstash_plugin/tests/test_plugin.py
@@ -13,93 +13,39 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-
 import os
-import unittest
 import subprocess
 
-from cloudify.workflows import local
-
-DEFAULT = """
-input { stdin { type => "stdin-type"}}
-output { stdout { debug => true debug_format => "json"}}
-"""
-
-LOGSTASH = """
-inputs {
-    generator {
-        tags => ["event"]
-        count => 0
-        message => "Test Event Message"
-    }
-    generator {
-        tags => ["log"]
-        count => 0
-        message => "Test Log Message"
-    }
-}
-
-outputs {
-    rabbitmq {
-        if "event" in [tags] {
-            exchange => ""
-            exchange_type => "direct"
-            key => "cloudify-events"
-            host => "MGMT_IP"
-            durable => "true"
-            exclusive => "false"
-        }
-    }
-    rabbitmq {
-        if "log" in [tags] {
-            exchange => ""
-            exchange_type => "direct"
-            key => "cloudify-logs"
-            host => "MGMT_IP"
-            durable => "true"
-            exclusive => "false"
-        }
-    }
-    stdout {}
-}
-"""
+from logstash_test_utils import (
+    LogstashTestUtils,
+    DEFAULT_UBUNTU_UNINSTALL,
+    DEFAULT_LOGSTASH_STOP,
+    DEFAULT_LOGSTASH_CONFIG_PATH
+)
+# from cloudify import ctx
 
 
-class TestPlugin(unittest.TestCase):
+class TestLogstashPlugin(LogstashTestUtils):
 
-    def setUp(self):
-        # build blueprint path
-        blueprint_path = os.path.join(os.path.dirname(__file__),
-                                      'blueprint', 'blueprint.yaml')
-
-        # inject input from test
-        inputs = {}
-
-        # setup local workflow execution environment
-        self.env = local.init_env(blueprint_path,
-                                  name=self._testMethodName,
-                                  inputs=inputs)
-
-    def test_install(self):
-
-        # execute install workflow
+    def test_install_static_clean(self):
+        inputs = self.get_static_config_inputs()
+        self._set_up(inputs)
+        self.addCleanup(subprocess.call, DEFAULT_UBUNTU_UNINSTALL)
         self.env.execute('install', task_retries=10)
-
+        self.addCleanup(subprocess.call, DEFAULT_LOGSTASH_STOP)
         logstash_started = subprocess.call(
             "sudo service logstash status", shell=True)
-
         self.assertIn('started', logstash_started)
-
-        with open('/etc/logstash/conf.d/default', 'r') as default:
-            self.assertEqual(default.read(), DEFAULT)
-
-        with open('/etc/logstash/conf.d/logstash.conf', 'r') as logstash:
-            self.assertEqual(logstash.read(), LOGSTASH)
+        self.addCleanup(os.remove, DEFAULT_LOGSTASH_CONFIG_PATH)
+        with open(DEFAULT_LOGSTASH_CONFIG_PATH, 'r') as default:
+            self.assertEqual(default.read(), self.get_config())
 
     def test_uninstall(self):
-        # execute install workflow
+        self.addCleanup(subprocess.call, DEFAULT_UBUNTU_UNINSTALL)
+        self.addCleanup(subprocess.call, DEFAULT_LOGSTASH_STOP)
+        self.addCleanup(os.remove, DEFAULT_LOGSTASH_CONFIG_PATH)
+        self.env.execute('install', task_retries=10)
         self.env.execute('uninstall', task_retries=10)
-
         logstash_stopped = subprocess.call(
             "sudo service logstash status", shell=True)
         self.assertNotIn('started', logstash_stopped)
